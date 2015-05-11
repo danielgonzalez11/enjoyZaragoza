048155bf5ed8bf8b819621fd25c58a7e32a15a92
daniel <danielzgz11@gmail.com>
2015-05-11 12:35:51 +0200

    add event

---
+++ Quaver/App/Controller/addEvent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Quaver\App\Controller;
+
+use Quaver\Core\Controller;
+use Quaver\Core\Lang;
+use Quaver\App\Model\Event;
+
+class addEvent extends Controller
+{   
+	public function addEventAction(){//cargar evento
+
+		$this->render();
+    }
+}
